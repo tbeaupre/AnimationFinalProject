41ea906733d06e778b2a05a0b86fa88744758e95
Tyler Beaupre <tylerbeaupre4@gmail.com>
2017-04-21 00:48:17 -0400

    Big Bang

---
+++ Assets/Stem.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stem : MonoBehaviour {
+	public Stem stemPrefab;
+	int age = 0;
+	public List<Stem> children = new List<Stem>();
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Init (Stem stemPrefab, List<Stem> children)
+	{
+		this.stemPrefab = stemPrefab;
+		foreach (Stem child in children)
+		{
+			this.children.Add(child);
+			child.transform.SetParent(this.transform);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void GrowChildren()
+	{
+		Debug.Log("Growing Children");
+		foreach (Stem child in children)
+		{
+			child.Grow();
+		}
+	}
+
+	public virtual void Grow ()
+	{
+		Debug.Log("Stem Growing");
+		age++;
+		GrowChildren();
+
+		Stem stemClone = Instantiate<Stem>(stemPrefab, transform.position, transform.rotation);
+		stemClone.transform.SetParent(this.transform);
+		stemClone.Init(stemPrefab, children);
+		stemClone.transform.Translate(0, 0.4f, 0);
+		this.children.Clear();
+		this.children.Add(stemClone);
+	}
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+	int dt = 60;
+	int timer = 1;
+	//int bend = 180;
+	public Stem root;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer++;
+		if (timer % dt == 0)
+		{
+			Debug.Log("Timer Activated");
+			if (root)
+			{
+				root.Grow();
+			}
+			//if (timer == bend)
+			//{
+			//	root.Bend();
+			//}
+		}
+	}
+}
