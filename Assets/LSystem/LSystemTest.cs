7ed8b96a8e50436ca23d0ef5038f02bd582ff437
Tyler Beaupre <tylerbeaupre4@gmail.com>
2017-05-02 13:00:57 -0400

    LSystemTest shouldn't be an issue anymore...

--- Assets/LSystem/LSystemTest.cs
+++ Assets/LSystem/LSystemTest.cs
@@ -4,34 +4,21 @@ using System.Collections.Generic;
 
 public class LSystemTest : MonoBehaviour
 {
-	LSystem lSystem;
-
 	// Use this for initialization
-	void Start ()
-	{
-		string start = "X";
-		float pitch = 35;
-		float yaw = 70;
-		float roll = 35;
-		int maxN = 6;
-		RuleSet rules = new RuleSet();
-		rules.AddRule("X", 0.32f, "[[//+F(0)X]\\+F(0)X]\\-F(0)X");
-		rules.AddRule("X", 0.32f, "[\\+F(0)X]\\-F(0)X");
-		rules.AddRule("X", 0.32f, "[/+F(0)X]/-F(0)X");
-		rules.AddRule("X", 0.03f, "\\-F(0)X");
-		rules.AddRule("X", 0.03f, "/-F(0)X");
-		this.lSystem = new LSystem(start, rules, pitch, yaw, roll, maxN);
-
-		for (int n = 0; n < 10; n++)
-		{
-			Debug.Log(string.Format("n{0} = {1}", n, lSystem.GetResult(n)));
-		}
-	}
+	void Start () {}
 	
 	// Update is called once per frame
-	void Update ()
-	{
+	void Update () {}
 
+	public void PrintLSystem(LSystem lSystem)
+	{
+		if (lSystem != null)
+		{
+			for (int n = 0; n < 10; n++)
+			{
+				Debug.Log(string.Format("n{0} = {1}", n, lSystem.GetResult(n)));
+			}
+		}
 	}
 }
 
